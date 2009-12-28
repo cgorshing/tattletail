d84497d73f1d96f99a9112a001f61700bd886d57
ken.clark01 <ken.clark01@4130f082-8417-11dd-b2bb-736fb4d8a922>
2009-12-28 13:01:24 +0000

    Removed because no longer needed.

--- grails-app/services/com/brightdome/tattletale/service/parsers/StatusReportParser.groovy
+++
@@ -1,8 +0,0 @@
-package com.brightdome.tattletale.service.parsers
-
-import com.brightdome.tattletale.domain.Projects
-
-public interface StatusReportParser
-{
-	public Projects parse(String xml)
-}
