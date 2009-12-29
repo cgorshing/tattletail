7dcd99053683b1ff17c74faf3d2a96b959ab8704
ken.clark01 <ken.clark01@4130f082-8417-11dd-b2bb-736fb4d8a922>
2009-12-29 12:40:49 +0000

    Refactored classes to be more Groovy styled.

--- grails-app/services/com/brightdome/tattletale/service/BuildServerService.groovy
+++ grails-app/services/com/brightdome/tattletale/service/BuildServerService.groovy
@@ -8,29 +8,28 @@ public class BuildServerService
 	public List update()
 	{
 		def projects = []
-		BuildServer.list().each { projects += update ( it ) }
+		BuildServer.list().each { projects += updateBuildStatus( it ) }
 		return projects
 	}
 	
-	private List update(BuildServer buildServer)
+	private List updateBuildStatus( BuildServer buildServer )
 	{
 		def projects = new XmlParser().parse( buildServer.url )
 		def moniteredProjects = []
 		
-		for (Build build: buildServer.builds)
-        {
-			  def project = projects.find { it.'@name' ==~ build.name }
+		buildServer.builds.each { build -> 
+			def project = projects.find { p -> p.'@name' ==~ build.name }
 			
-			  if ( project != null )
-			  {
-				  build.status = project.'@lastBuildStatus'
-				  build.activity = project.'@activity'
-				  build.label = project.'@lastBuildLabel'
-				  build.url = project.'@webUrl'
-				  build.time = new Date().parse( "yyyy-MM-dd'T'HH:mm:ss",  project.'@lastBuildTime'.replace( "Z", "") )
-				  moniteredProjects.add( build )
-			  }
-        }
+			if ( project != null )
+			{
+				build.status = project.'@lastBuildStatus'
+				build.activity = project.'@activity'
+				build.label = project.'@lastBuildLabel'
+				build.url = project.'@webUrl'
+				build.time = new Date().parse( "yyyy-MM-dd'T'HH:mm:ss",  project.'@lastBuildTime'.replace( "Z", "") )
+				moniteredProjects += build
+			}
+		}
 		
 		return moniteredProjects.sort { it.sequence }
 	}
